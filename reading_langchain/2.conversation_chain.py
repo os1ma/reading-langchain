3c374b8b0490312800d8ed6e66d2877006562c2a
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-06-27 12:36:05 +0900

    Update 2.conversation_chain.py

--- reading_langchain/2.conversation_chain.py
+++ reading_langchain/2.conversation_chain.py
@@ -12,17 +12,17 @@ openai.log = "debug"
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
 memory = ConversationBufferMemory()
-memory.chat_memory.add_user_message("hi!")
-memory.chat_memory.add_ai_message("whats up?")
+memory.chat_memory.add_user_message("Hi! I'm Oshima.")
+memory.chat_memory.add_ai_message("Whats up?")
 
 # Memory のチュートリアルの通り以下のように ConversationChain を使うと、
 # role: user に全ての履歴が含まれてしまい、Chat Completions API の使い方として適切ではなくなる
 conversation = ConversationChain(llm=chat, memory=memory)
-conversation_result = conversation.predict(input="Hi there!")
+conversation_result = conversation.predict(input="Do you know my name?")
 print(conversation_result)
 
 # 自前で履歴を与えると、role: assistant を活用した、Chat Completions API として適切なプロンプトになる
 messages = memory.chat_memory.messages
-messages.append(HumanMessage(content="Hi there!"))
+messages.append(HumanMessage(content="Do you know my name?"))
 raw_chat_result = chat(messages)
 print(raw_chat_result.content)
